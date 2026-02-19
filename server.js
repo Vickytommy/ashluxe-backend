2071fcf69fdf58bf1470efd7ffa3b211a2e58201
Vickytommy <vickytommy785@gmail.com>
2026-02-19 14:00:03 +0100

    new 3

--- server.js
+++ server.js
@@ -349,7 +349,7 @@ app.post('/shopify_cart_update', async (req, res) => {
       attr => attr.name === "wishlistShareId"
     )?.value;
 
-    console.log('THE req - ', order);
+    console.log('THE req & note- ', order, wishlistShareId);
 });
 
 app.get('/ashluxury', async (req, res) => {
